52fecb73d3fbf63b06a2d6750e60370d7cbdeed5
Ryan DiSalle <ryandisalle@yahoo.com>
2020-07-30 15:08:30 -0400

    final

--- server.js
+++ server.js
@@ -1,18 +1,52 @@
 require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
-
-console.log(process.env.API_TOKEN)
+const helmet = require('helmet')
+const movie = require('./movies-data-small.json')
+const cors = require('cors')
 
 const app = express()
 
 app.use(morgan('dev'))
+app.use(helmet())
+app.use(cors())
 
-function handleGetMovie(req, res) {
+app.use(function validateBearerToken(req, res, next) {
+    const apiToken = process.env.API_TOKEN
+    const authToken = req.get('Authorization')
+    console.log('validate bearer token middleware')
+    if (!authToken || authToken.split(' ')[1] !== apiToken) {
+      return res.status(401).json({ error: 'Unauthorized request' })
+      }
+    // move to the next middleware
+    next()
+})
 
-    }
-    
-    app.get('/movie', handleGetMovie)
+app.get('/movie', function handleGetMovie(req, res) {
+  let results = movie
+
+  if (req.query.genre) {
+    results = results.filter(movie =>
+      movie.genre.toLowerCase().includes(req.query.genre.toLowerCase())
+    )
+  }
+
+  if (req.query.country) {
+    results = results.filter(movie =>
+      movie.country.toLowerCase().includes(req.query.country.toLowerCase())
+    )
+  }
+
+  if (req.query.avg_vote) {
+    results = results.filter(movie => 
+      Number(movie.avg_vote) >= Number(req.query.avg_vote)
+    )
+  }
+
+  res
+  .json(results);
+  });
+  
 
 const PORT = 8000
 
